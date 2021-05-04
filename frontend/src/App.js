e25cee22681ec37ff4f0b1c5bafd0611b2e4911b
DesmondM <desmondmpofu@gmail.com>
2021-05-04 21:31:47 +0200

    product screen additions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
         </div>
       </header>
       <main>
-        <Route path="product/:id" component={ProductScreen}></Route>
+        <Route path="/product/:id" component={ProductScreen}></Route>
         <Route path="/" component={HomeScreen} exact></Route>
        
       </main>
